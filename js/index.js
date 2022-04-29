b157e6d3c83ae03c48053dc9ffb8bacb48db9e83
FherAmableRicse <amablericsefher@gmail.com>
2022-04-28 22:50:52 -0500

    index js agregado

---
+++ js/index.js
@@ -0,0 +1,52 @@
+'use strict';
+const documentReady = () => {
+  const formCalculadora = document.getElementById('formCalculadora');
+ 
+   const calcular = (e) => {
+     e.preventDefault();
+
+     const numero1 = parseInt(document.getElementById('numero1').value);
+     const operador = document.getElementById('operador').value;
+     const numero2 = parseInt(document.getElementById('numero2').value);
+     const contenedorError = document.getElementById('contenedorError');
+     const contenedorResultado = document.getElementById('contenedorResultado');
+
+    if(isNaN(numero1) || isNaN(numero2)) {
+      contenedorError.innerHTML = 'Completar el formulario';
+    }
+    else {
+      let resultado = 0;
+      contenedorError.innerHTML= '';
+     switch (operador) {
+       case '+': 
+       resultado = numero1 + numero2;
+         break;
+       case '-':
+        resultado = numero1 - numero2;
+         break;
+       case '*':
+        resultado = numero1 * numero2;
+         break;
+       case '/': 
+       if(numero1 === 0 || numero2 === 0) {
+        contenedorError.innerHTML = 'Los operadores no pueden ser cero';
+       }
+       else {
+         resultado = numero1 / numero2;
+        }
+         break;
+       default:
+        contenedorError.innerHTML = 'Seleccionar una operacion valida';
+         break;
+     }
+     contenedorResultado.innerHTML = resultado;
+     setTimeout(()=>{
+      contenedorResultado.innerHTML = '';
+     },2000);
+    }
+   }; 
+  formCalculadora.addEventListener('submit', calcular);
+}
+document.addEventListener('DOMContentLoaded', documentReady);
+
+
